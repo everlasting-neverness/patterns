9ae8e4dee75d4f16bb65e07bc8dce7b2014a0486
Vasily <gerasimovv1990@gmail.com>
2020-03-30 14:15:26 +0300

    update

---
+++ behavioral/20-command.js
@@ -0,0 +1,55 @@
+// Помогает инкапсулировать некоторые действия и необходимые для них данные.
+// Превращает запросы в объекты, что позволяет передавать их как аргументы в методы.
+
+class Driver {
+    constructor(command) {
+        this.command = command;
+    }
+
+    execute() {
+        this.command.execute();
+    }
+}
+
+class Engine {
+    constructor() {
+        this.state = false;
+    }
+
+    on() {
+        this.state = true;
+    }
+
+    off() {
+        this.state = false;
+    }
+}
+
+class OnStartCommand {
+    constructor(engine) {
+        this.engine = engine;
+    }
+
+    execute() {
+        this.engine.on();
+    }
+}
+
+class onSwitchOffCommand {
+    constructor(engine) {
+        this.engine = engine;
+    }
+
+    execute() {
+        this.engine.off();
+    }
+}
+
+
+const engine = new Engine();
+console.log(engine);
+
+const onStartCommand = new OnStartCommand(engine);
+const driver = new Driver(onStartCommand);
+driver.execute();
+console.log(engine);
